5aee2813ec38a77e7dca72bf84594c88de4ac842
Francisco Sousa <francisco.sousa@hey.com>
2020-12-09 01:45:16 +0000

    Fix

--- utils/image.js
+++ utils/image.js
@@ -36,30 +36,30 @@ module.exports = async (src, alt, klass, responsive = false) => {
   );
 
   return `
-  <picture>
-    <source type="image/webp" data-srcset="${srcset["webp"]}">
-    <img
-      alt="${alt}"
-      src="${base64Placeholder}"
-      data-src="${lowestSrc.url}"
-      data-sizes="(min-width: 1024px) 1024px, 100vw"
-      data-srcset="${srcset["jpeg"]}"
-      class="lazy ${klass}">
-  </picture>
-
-  <noscript>
     <picture>
-      <source type="image/webp" srcset="${srcset["webp"]}">
+      <source type="image/webp" data-srcset="${srcset["webp"]}">
       <img
-        loading="lazy"
         alt="${alt}"
-        src="${lowestSrc.url}"
-        sizes="(min-width: 1024px) 1024px, 100vw"
-        srcset="${srcset["jpeg"]}"
-        width="${lowestSrc.width}"
-        height="${lowestSrc.height}"
-        class="${klass}">
+        src="${base64Placeholder}"
+        data-src="${lowestSrc.url}"
+        data-sizes="(min-width: 1024px) 1024px, 100vw"
+        data-srcset="${srcset["jpeg"]}"
+        class="lazy ${klass}">
     </picture>
-  </noscript>
-`;
+
+    <noscript>
+      <picture>
+        <source type="image/webp" srcset="${srcset["webp"]}">
+        <img
+          loading="lazy"
+          alt="${alt}"
+          src="${lowestSrc.url}"
+          sizes="(min-width: 1024px) 1024px, 100vw"
+          srcset="${srcset["jpeg"]}"
+          width="${lowestSrc.width}"
+          height="${lowestSrc.height}"
+          class="${klass}">
+      </picture>
+    </noscript>
+  `;
 };
