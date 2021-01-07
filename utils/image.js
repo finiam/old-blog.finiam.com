9f7730e3557af2b3aac06043470d3ad34a54a1ef
Francisco Sousa <francisco.sousa@hey.com>
2021-01-07 19:13:29 +0000

    Revert "Reinstate @11ty/eleventy-img (#29)"
    This reverts commit def9751ce3c7becb351fbb5c619fbd9627cf2c79.

--- utils/image.js
+++ utils/image.js
@@ -1,8 +1,9 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
-const Image = require("@11ty/eleventy-img");
+const hash = require("./hash");
 
+const CACHE = {};
 const IMAGES_ROOT = "./static/";
 const OUTPUT_PATH = "./_output/optimized_images/";
 const URL_PATH = "/optimized_images/";
@@ -21,6 +22,72 @@ class="${klass}">
 </picture>`;
 }
 
+async function saveSharpImage(sharpImage, fileName) {
+  if (fs.existsSync(fileName)) return;
+
+  await sharpImage.toFile(fileName);
+}
+
+async function generateImage(sharpImage, fileName, width = null) {
+  sharpImage.resize({
+    width: width ? width : defaultImgWidth,
+  });
+
+  if (!fs.existsSync(OUTPUT_PATH))
+    fs.mkdirSync(OUTPUT_PATH, { recursive: true });
+
+  await Promise.all([
+    saveSharpImage(sharpImage, path.join(OUTPUT_PATH, `${fileName}.avif`)),
+    saveSharpImage(sharpImage, path.join(OUTPUT_PATH, `${fileName}.jpeg`)),
+  ]);
+}
+
+async function generateImages(inputPath, imageHash, responsive) {
+  const sharpImage = await sharp(inputPath);
+  const imageWidth = (await sharpImage.metadata()).width;
+  const defaultImgWidth = imageWidth > 1280 ? 1280 : imageWidth;
+  const widths = responsive ? [320, 480, 800, 1024, 1280] : [defaultImgWidth];
+
+  await Promise.all(
+    widths.map((width) =>
+      generateImage(sharpImage, `${imageHash}-${width}`, width),
+    ),
+  );
+
+  const placeholder = await sharp(inputPath)
+    .resize({ width: 64 })
+    .blur()
+    .toBuffer();
+
+  return {
+    jpeg: {
+      url: path.join(URL_PATH, `${imageHash}.jpeg`),
+      srcset: widths
+        .map(
+          (width) =>
+            `${path.join(URL_PATH, `${imageHash}-${width}.jpeg`)} ${width}w`,
+        )
+        .join(", "),
+      sizes: widths
+        .map((width) => `(max-width: ${width}px) ${width - 20}px`)
+        .join(", "),
+    },
+    avif: {
+      url: path.join(URL_PATH, `${imageHash}.avif`),
+      srcset: widths
+        .map(
+          (width) =>
+            `${path.join(URL_PATH, `${imageHash}-${width}.avif`)} ${width}w`,
+        )
+        .join(", "),
+      sizes: widths
+        .map((width) => `(max-width: ${width}px) ${width}px`)
+        .join(", "),
+    },
+    placeholder: `data:image/png;base64,${placeholder.toString("base64")}`,
+  };
+}
+
 module.exports = async (src, alt, klass, responsive = false) => {
   try {
     if (!alt) throw new Error(`Missing \`alt\` from: ${src}`);
@@ -30,21 +97,40 @@ module.exports = async (src, alt, klass, responsive = false) => {
     if (process.env.NODE_ENV !== "production")
       return skipOptimization(src, alt, klass);
 
-    const metadata = await Image(path.join(IMAGES_ROOT, src), {
-      formats: ["avif", "jpeg"],
-      outputDir: OUTPUT_PATH,
-      urlPath: URL_PATH,
-      widths: responsive ? [320, 480, 800, 1024, 1280] : [null],
-    });
+    const inputPath = path.join(IMAGES_ROOT, src);
+    const imageHash = (await hash(inputPath)).slice(0, 20);
+    let imageStats = CACHE[imageHash];
 
-    const imageAttributes = {
-      alt,
-      class: klass,
-      loading: "lazy",
-      decoding: "async",
-    };
+    if (!imageStats) {
+      imageStats = await generateImages(inputPath, imageHash, responsive);
+      CACHE[hash] = imageStats;
+    }
 
-    return Image.generateHTML(metadata, imageAttributes);
+    return `
+<picture>
+<source type="image/avif" data-srcset="${imageStats.avif.srcset}" data-sizes="${imageStats.avif.sizes}">
+<img
+alt="${alt}"
+src="${imageStats.placeholder}"
+data-src="${imageStats.jpeg.url}"
+data-srcset="${imageStats.jpeg.srcset}"
+data-sizes="${imageStats.jpeg.sizes}"
+class="lazy ${klass}">
+</picture>
+
+<noscript>
+<picture>
+<source type="image/avif" srcset="${imageStats.avif.srcset}" sizes="${imageStats.avif.sizes}">
+<img
+loading="lazy"
+alt="${alt}"
+src="${imageStats.jpeg.url}"
+srcset="${imageStats.jpeg.srcset}"
+sizes="${imageStats.jpeg.sizes}"
+class="${klass}">
+</picture>
+</noscript>
+  `;
   } catch (error) {
     console.error(`Error on ${src}`);
 
